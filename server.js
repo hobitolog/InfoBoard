91fbf32226fbf0b58b2ec2590818116ccf19e624
kosert <robert.kosakowski@student.put.poznan.pl>
2018-04-18 18:24:35 +0200

    nodeipc

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 var path = require('path')
+var exec = require('child_process').exec
+
+var dir = path.join(__dirname, "/electro.js")
+exec('electron "' + dir + '"')
+
+var ipc = require('node-ipc')
+ipc.config.id = 'InfoBoardServer'
+ipc.config.retry = 1500
+
+ipc.serve(function () {
+    ipc.server.on('message', function (data, socket) {
+        console.log(data)
+        ipc.server.emit(socket, 'message', 'https://github.com');
+    })
+})
+ipc.server.start();
 
 var schedule = require('./schedule')
 schedule.load()
@@ -8,7 +24,7 @@ var express = require('express')
 var morgan = require('morgan')
 var app = express()
 
-app.use(express.static(path.join(__dirname + '/public')))
+app.use(express.static(path.join(__dirname, '/public')))
 app.use(morgan('dev'))
 app.use(bodyParser())
 
@@ -17,24 +33,24 @@ app.get('/', (req, res) => {
 })
 
 app.get('/schedule', (req, res) => {
-     res.json(schedule.getNameList())
+    res.json(schedule.getNameList())
 })
 
 app.post('/addSchedule', (req, res) => {
 
     var startTime = valueOrAsterisk(req.body.startSeconds) + " " +
-    valueOrAsterisk(req.body.startMinutes) + " " + 
-    valueOrAsterisk(req.body.startHours) + " " +
-    valueOrAsterisk(req.body.startDoM) + " " +
-    valueOrAsterisk(req.body.startMonths) + " " +
-    valueOrAsterisk(req.body.startDoW)
+        valueOrAsterisk(req.body.startMinutes) + " " +
+        valueOrAsterisk(req.body.startHours) + " " +
+        valueOrAsterisk(req.body.startDoM) + " " +
+        valueOrAsterisk(req.body.startMonths) + " " +
+        valueOrAsterisk(req.body.startDoW)
 
     var stopTime = valueOrAsterisk(req.body.stopSeconds) + " " +
-    valueOrAsterisk(req.body.stopMinutes) + " " + 
-    valueOrAsterisk(req.body.stopHours) + " " +
-    valueOrAsterisk(req.body.stopDoM) + " " +
-    valueOrAsterisk(req.body.stopMonths) + " " +
-    valueOrAsterisk(req.body.stopDoW)
+        valueOrAsterisk(req.body.stopMinutes) + " " +
+        valueOrAsterisk(req.body.stopHours) + " " +
+        valueOrAsterisk(req.body.stopDoM) + " " +
+        valueOrAsterisk(req.body.stopMonths) + " " +
+        valueOrAsterisk(req.body.stopDoW)
 
     schedule.addToSchedule({
         "name": req.body.name,
@@ -51,9 +67,8 @@ app.listen(80, function () {
     console.log("Server started")
 })
 
-function valueOrAsterisk(value)
-{
-    if(value)
+function valueOrAsterisk(value) {
+    if (value)
         return value
     else
         return "*"
